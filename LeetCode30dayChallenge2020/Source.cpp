4979992e705c573c8761680d48166d849d4896d6
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-04-18 12:17:04 +0200

    day18 DP added

--- LeetCode30dayChallenge2020/Source.cpp
+++ LeetCode30dayChallenge2020/Source.cpp
@@ -709,7 +709,60 @@ namespace day17
 		std::cout << day17::Solution().numIslands(grid);
 	}
 }
-namespace day18
+namespace day18 // DP in simple case where you only move east and south
+{
+	class Solution {
+	public:
+		int minPathSum(std::vector<std::vector<int>>& grid)
+		{
+			if (grid.size() == 0 && grid[0].size() == 0) return 0;
+			size_t width = grid[0].size();
+			size_t height = grid.size();
+			for (size_t x = 0; x < width ; x++)
+			{
+				for (size_t y = 0; y < height ; y++)
+				{
+					if (x != 0 && y != 0) grid[y][x] = std::min(grid[y-1][x],grid[y][x-1])+grid[y][x];
+					else if (x == 0 && y != 0) grid[y][x] = grid[y - 1][x] + grid[y][x];
+					else if (x != 0 && y == 0) grid[y][x] = grid[y][x-1] + grid[y][x];					
+				}
+			}
+			return grid[height - 1][width - 1];
+		}
+	};
+	void RunExample()
+	{
+		std::vector<std::vector<int>> vec = {
+			{1, 2, 1},
+			{3, 1, 2},
+			{5, 5, 1}
+		};
+
+		std::vector<std::vector<int>> vec2 = {
+			{5 , 4 , 2 , 9 , 6 , 0 , 3 , 5 , 1 , 4 , 9 , 8 , 4 , 9 , 7 , 5 , 1} ,
+			{3 , 4 , 9 , 2 , 9 , 9 , 0 , 9 , 7 , 9 , 4 , 7 , 8 , 4 , 4 , 5 , 8} ,
+			{6 , 1 , 8 , 9 , 8 , 0 , 3 , 7 , 0 , 9 , 8 , 7 , 4 , 9 , 2 , 0 , 1} ,
+			{4 , 0 , 0 , 5 , 1 , 7 , 4 , 7 , 6 , 4 , 1 , 0 , 1 , 0 , 6 , 2 , 8} ,
+			{7 , 2 , 0 , 2 , 9 , 3 , 4 , 7 , 0 , 8 , 9 , 5 , 9 , 0 , 1 , 1 , 0} ,
+			{8 , 2 , 9 , 4 , 9 , 7 , 9 , 3 , 7 , 0 , 3 , 6 , 5 , 3 , 5 , 9 , 6} ,
+			{8 , 9 , 9 , 2 , 6 , 1 , 2 , 5 , 8 , 3 , 7 , 0 , 4 , 9 , 8 , 8 , 8} ,
+			{5 , 8 , 5 , 4 , 1 , 5 , 6 , 6 , 3 , 3 , 1 , 8 , 3 , 9 , 6 , 4 , 8} ,
+			{0 , 2 , 2 , 3 , 0 , 2 , 6 , 7 , 2 , 3 , 7 , 3 , 1 , 5 , 8 , 1 , 3} ,
+			{4 , 4 , 0 , 2 , 0 , 3 , 8 , 4 , 1 , 3 , 3 , 0 , 7 , 4 , 2 , 9 , 8} ,
+			{5 , 9 , 0 , 4 , 7 , 5 , 7 , 6 , 0 , 8 , 3 , 0 , 0 , 6 , 6 , 6 , 8} ,
+			{0 , 7 , 1 , 8 , 3 , 5 , 1 , 8 , 7 , 0 , 2 , 9 , 2 , 2 , 7 , 1 , 5} ,
+			{1 , 0 , 0 , 0 , 6 , 2 , 0 , 0 , 2 , 2 , 8 , 0 , 9 , 7 , 0 , 8 , 0} ,
+			{1 , 1 , 7 , 2 , 9 , 6 , 5 , 4 , 8 , 7 , 8 , 5 , 0 , 3 , 8 , 1 , 5} ,
+			{8 , 9 , 7 , 8 , 1 , 1 , 3 , 0 , 1 , 2 , 9 , 4 , 0 , 1 , 5 , 3 , 1} ,
+			{9 , 2 , 7 , 4 , 8 , 7 , 3 , 9 , 2 , 4 , 2 , 2 , 7 , 8 , 2 , 6 , 7} ,
+			{3 , 8 , 1 , 6 , 0 , 4 , 8 , 9 , 8 , 0 , 2 , 5 , 3 , 5 , 5 , 7 , 5} ,
+			{1 , 8 , 2 , 5 , 7 , 7 , 1 , 9 , 9 , 8 , 9 , 2 , 4 , 9 , 5 , 4 , 0} ,
+			{3 , 4 , 4 , 1 , 5 , 3 , 3 , 8 , 8 , 6 , 3 , 5 , 3 , 8 , 7 , 1 , 3}
+		};
+		int sol = Solution().minPathSum(vec);
+	}
+}
+namespace day18b // With Dijkstra in case you want to move in all directions
 {
 	class Solution {
 	public:
