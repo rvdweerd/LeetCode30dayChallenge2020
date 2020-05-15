b68f3eb19d8927ee1b2c228f92800867e42119fe
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-05-15 17:12:14 +0200

    May day15 solved

--- LeetCode30dayChallenge2020/Source.cpp
+++ LeetCode30dayChallenge2020/Source.cpp
@@ -17,39 +17,39 @@
 
 size_t allocated=0;
 
-void* operator new(size_t size)
-{
-	std::cout << "Allocating " << size << "bytes\n";
-	allocated += size;
-	std::cout << " Total allocated: " << allocated << "bytes\n";
-	return std::malloc(size);
-}
-void operator delete(void* ptr, size_t size)
-{
-	std::cout << "Freeing " << size << "bytes\n";
-	allocated -= size;
-	std::cout << " Total allocated: " << allocated << "bytes\n";
-	free(ptr);
-}
-
-void operator delete[](void* ptr)//, size_t size)
-{
-	int count = 1;
-	//const char* p = static_cast<const char*>(ptr);
-	const char* p = reinterpret_cast<const char*>(ptr);
-	for (; *p != 0; p++)
-	{
-		count++;
-	}
-	std::cout << "Freeing " << count << " bytes\n";
-	allocated += count;
-	std::cout << " Total allocated: " << count << "bytes\n";
-	std::free(ptr);
-}
+//void* operator new(size_t size)
+//{
+//	std::cout << "Allocating " << size << "bytes\n";
+//	allocated += size;
+//	std::cout << " Total allocated: " << allocated << "bytes\n";
+//	return std::malloc(size);
+//}
+//void operator delete(void* ptr, size_t size)
+//{
+//	std::cout << "Freeing " << size << "bytes\n";
+//	allocated -= size;
+//	std::cout << " Total allocated: " << allocated << "bytes\n";
+//	free(ptr);
+//}
+//
+//void operator delete[](void* ptr)//, size_t size)
+//{
+//	int count = 1;
+//	//const char* p = static_cast<const char*>(ptr);
+//	const char* p = reinterpret_cast<const char*>(ptr);
+//	for (; *p != 0; p++)
+//	{
+//		count++;
+//	}
+//	std::cout << "Freeing " << count << " bytes\n";
+//	allocated += count;
+//	std::cout << " Total allocated: " << count << "bytes\n";
+//	std::free(ptr);
+//}
 
 int main()
 {
-	May_day14::RunExample();
+	May_day15::RunExample();
 	//LC36::RunExample();
 
 	std::cin.get();
