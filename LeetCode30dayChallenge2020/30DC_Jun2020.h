e99e170cbc873a0a85c65d7eac5a9b9b8e1afc0a
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-06-16 20:49:14 +0200

    June day 16 (LC468 Regex / Validate IP Address)

--- LeetCode30dayChallenge2020/30DC_Jun2020.h
+++ LeetCode30dayChallenge2020/30DC_Jun2020.h
@@ -675,3 +675,28 @@ namespace Jun_day14 // LC787 Cheapest Flights with K stops
 		ans = Solution().findCheapestPrice(4, flights, 3, 0, 3); //-1
 	}
 }
+#include <regex>
+namespace Jun_day16 // LC468 Validate IP Address
+{
+	class Solution {
+	public:
+		std::string validIPAddress(std::string IP) 
+		{
+			std::regex ipv_4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])"), ipv_6("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");
+			if (regex_match(IP, ipv_4)) 
+				return "IPv4";
+			else if (regex_match(IP, ipv_6)) 
+				return "IPv6";
+			return "Neither"; 
+		}
+	};
+	void RunExample()
+	{
+		std::string address;
+		std::string ans;
+
+		address = "2001:0db8:85a3:0:0:8A2E:0370:7334";
+		ans = Solution().validIPAddress(address);
+
+	}
+}
